a002bd1067af5d986f0c6dd57621ee1da047f4a0
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-09-30 13:18:34 +0500

    connected to mongo db atlas with code snipet to vs

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://developer-rak:rkkalhoro6576@gqlrak.m3le9.mongodb.net/?retryWrites=true&w=majority&appName=gqlrak');
+mongoose.connection.once('open', () => {
+   console.log('connected to database');
+})
 
 const app = express();
 
